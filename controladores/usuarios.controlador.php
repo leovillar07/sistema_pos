5830bd86b665966559c1a176b2ff2e5fa4e7a58a
Leonardo Villar <201614025@uns.edu.pe>
2021-03-20 17:46:44 -0500

    agregar Usuario

--- controladores/usuarios.controlador.php
+++ controladores/usuarios.controlador.php
@@ -9,7 +9,7 @@ class UsuarioControlador
             if (preg_match('/^[a-zA-Z0-9]+$/', $_POST["ingUsuario"]) &&
                 preg_match('/^[a-zA-Z0-9]+$/', $_POST["ingPassword"])) {
 
-                $tabla = "tb_usuarios";
+                $tabla = "usuarios";
                 $item = "usuario";
                 $valor = $_POST["ingUsuario"];
                 $respuesta = UsuarioModelo::MdlMostrarUsuarios($tabla, $item, $valor);
@@ -20,10 +20,7 @@ class UsuarioControlador
                 } else {
                     echo "<div class='alert alert-danger'>Usuario o Contraseña Erronea</div>";
                 }
-
             }
-
-
         }
     }
 
@@ -35,6 +32,22 @@ class UsuarioControlador
             if (!preg_match('/^[a-zA-Z0-9]+$/', $_POST["nuevoUsuario"])) $this->UsuarioError();
             if (!preg_match('/^[a-zA-Z0-9]+$/', $_POST["nuevoPassword"])) $this->passwordError();
 
+            $tabla = "usuarios";
+
+            $datos = array(
+                "nombre" => $_POST["nuevoNombre"],
+                "usuario" => $_POST["nuevoUsuario"],
+                "password" => $_POST["nuevoPassword"],
+                "perfil" => $_POST["nuevoPerfil"]
+            );
+
+            $respuesta = UsuarioModelo::mdlCrearUsuario($tabla, $datos);
+
+            if ($respuesta == "ok") {
+                $this->creacionExitosa();
+            } else {
+                $this->creacionErronea();
+            }
         }
     }
 
@@ -81,6 +94,43 @@ class UsuarioControlador
           swal({
             type: 'error',
             title: '¡La contraseña no puede ir vacia o llevar caracteres especiales!',
+            showConfirmButton: true,
+            confirmButtonText: 'Cerrar',
+            closeOnConfirm: false 
+          }).then((result)=>{
+            if (result.value){
+              window.location = 'usuarios';
+              }
+            })
+        </script>";
+    }
+
+    public function creacionExitosa()
+    {
+        echo
+        "<script>
+          swal({
+            type: 'success',
+            title: '¡Usuario Creado!',
+            text: 'Usuario Creado exitosamente',
+            showConfirmButton: true,
+            confirmButtonText: 'Cerrar',
+            closeOnConfirm: false 
+          }).then((result)=>{
+            if (result.value){
+              window.location = 'usuarios';
+              }
+            })
+        </script>";
+    }
+
+    public function creacionErronea()
+    {
+        echo
+        "<script>
+          swal({
+            type: 'error',
+            title: '¡Hubo un error al guardar el usuario, intente mas tarde!',
             showConfirmButton: true,
             confirmButtonText: 'Cerrar',
             closeOnConfirm: false 
--- modelos/usuarioModelo.modelo.php
+++ modelos/usuarioModelo.modelo.php
@@ -13,4 +13,23 @@ class UsuarioModelo
         $stmt->execute();
         return $stmt->fetch();
     }
+
+    public static function mdlCrearUsuario($tabla, $datos)
+    {
+
+
+        $stmt = Conexion::conectar()->prepare("insert into $tabla (nombre, usuario, password, perfil) values (:nombre, :usuario, :password, :perfil)");
+
+        $stmt->bindParam(":nombre", $datos["nombre"], PDO::PARAM_STR);
+        $stmt->bindParam(":usuario", $datos["usuario"], PDO::PARAM_STR);
+        $stmt->bindParam(":password", $datos["password"], PDO::PARAM_STR);
+        $stmt->bindParam(":perfil", $datos["perfil"], PDO::PARAM_STR);
+
+        if ($stmt->execute()) {
+            return "ok";
+        } else {
+            return "error";
+        }
+
+    }
 }
